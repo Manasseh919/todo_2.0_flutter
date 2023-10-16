5643bd156e86ffb265ed91a9574b5adc59750227
Manasseh919 <69004938+Manasseh919@users.noreply.github.com>
2023-10-16 09:33:12 +0000

    checkbox and listview dynamic

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -9,22 +9,35 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  //list of todo task
+  List todoList = [
+    ["Make Tutorial", false],
+    ["Do exercice", false],
+  ];
+
+  //checkbox tapped
+  void checkBoxChanged(bool? value, int index){
+    setState(() {
+      todoList[index][1]=!todoList[index][1];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.yellow[200],
       appBar: AppBar(
         title: Text('To Do'),
-     
       ),
-      body: ListView(
-        children: [
-          ToDoTile(
-            taskName: "Manasseh",
-              taskCompleted: true,
-              onChanged: (p0) => true,
-          )
-        ],
+      body: ListView.builder(
+        itemCount: todoList.length,
+        itemBuilder: (context, index)  {
+          return ToDoTile(
+            taskName: todoList[index][0],
+            taskCompleted: todoList[index][1],
+            onChanged: (value)=>checkBoxChanged(value,index),
+          );
+        },
       ),
     );
   }
